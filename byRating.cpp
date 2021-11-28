1ff498e1b259bc0b166672bab5d12a6a8c9e1f2d
alihshaikh <alihshaikh322@gmail.com>
2021-11-27 20:06:43 -0800

    made byRating pref algorithm include genre preferences as well

--- byRating.cpp
+++ byRating.cpp
@@ -7,15 +7,19 @@ using namespace std;
 
 byRating::byRating(vector<Movie> data, vector<string> answers) {
 	dataBase = data;
-	answers = userAns;
+	userAns = answers;
 }
 
 vector<Movie> byRating::getPreferences(vector<Movie> movieList, vector<string> answers) {
 	vector<Movie> returnVec;
 
 	for(int i = 0; i < movieList.size(); ++i) {
-		if(movieList.at(i).rating > 8.5) {
-			returnVec.push_back(movieList.at(i));
+		if(movieList.at(i).rating > 8.2) {
+			for(int j = 0; j < movieList.at(i).genre.size(); ++j) {
+				if(movieList.at(i).genre.at(j) == answers.at(1)) {
+					returnVec.push_back(movieList.at(i));
+				}
+			}
 		}
 	}
 	return returnVec;
